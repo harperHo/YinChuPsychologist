2dc59958081c1c9ca206ce3f9360a5d2fff4d24b
Harper <harper.h.ho@gmail.com>
2024-04-03 01:03:06 +0100

    fix: path of images

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@
 const nextConfig = {
   output: "export",
   basePath: "/YinChuPsychologist",
-  assetPrefix: "/YinChuPsychologist/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
