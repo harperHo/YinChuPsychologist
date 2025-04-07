373dd4ac94105253d43eeb2171410c1d0c5738c7
Harper <harper.h.ho@gmail.com>
2025-04-07 10:03:43 +0100

    feat: add favicon and title; load google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Script from "next/script";
 
 import { taipeiSans, genWanMin } from "./styles/fonts";
 import "./globals.css";
+
+const GA_ID = "G-66ZHT41ZDL";
 
 export default function RootLayout({
   children,
@@ -13,6 +16,28 @@ export default function RootLayout({
       lang="en"
       className={`${taipeiSans.variable} ${genWanMin.variable} font-tp-sans bg-sand text-text`}
     >
+      <head>
+        <title>劉映竺諮商心理師 Ying Chu Liu Counseling Psychologist</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        {/* Google Analytics Script 1: Load gtag.js */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        {/* Google Analytics Script 2: Initialize gtag */}
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
